8038b2f5e44ff6a63cb1077b226ac8c609b20d6b
Matt Thomas <matt@betweenbrain.com>
2013-12-13 14:00:15 -0500

    Flush out more of the post URL

--- component/site/views/k2search/tmpl/default.php
+++ component/site/views/k2search/tmpl/default.php
@@ -9,21 +9,23 @@
  * License    GNU GPL v3 or later
  */
 ?>
-	<form action="index.php" method="post">
-		<input type="text" name="term" size="32" />
-		<input type="hidden" name="option" value="com_k2search" />
-		<input type="hidden" name="view" value="k2search" />
-	</form>
+<form action="index.php" method="post">
+	<input type="hidden" name="option" value="com_k2search" />
+	<input type="hidden" name="view" value="k2search" />
+	<input type="hidden" name="layout" value="default" />
+	<input type="hidden" name="Itemid" value="<?php echo JSite::getMenu()->getActive()->id ?>" />
+	<input type="text" name="term" size="32" />
+</form>
 
 <h2>Results for "<?php echo $this->results['results']->term ?>"</h2>
 <p>Displaying <?php echo $this->results['results']->count ?> results</p>
 <?php unset($this->results['results']) ?>
 <ol>
-<?php foreach ($this->results as $result) : ?>
-	<li>
-	<h2><?php echo $result->title ?></h2>
-	<p><?php echo $result->introtext ?></p>
-	<p><?php echo print_r(json_decode($result->extra_fields), TRUE) ?></p>
-	</li>
-<?php endforeach; ?>
+	<?php foreach ($this->results as $result) : ?>
+		<li>
+			<h2><?php echo $result->title ?></h2>
+			<p><?php echo $result->introtext ?></p>
+			<p><?php // echo print_r(json_decode($result->extra_fields), TRUE) ?></p>
+		</li>
+	<?php endforeach; ?>
 </ol>
