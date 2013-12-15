8d01749b1e55541d0eb29caad4252fcc6ad3bf3a
Matt Thomas <matt@betweenbrain.com>
2013-12-15 08:47:39 -0500

    Adding example router.php

--- component/site/router.php
+++ component/site/router.php
@@ -1,17 +1,45 @@
 <?php
 
 /**
- * File			router.php
- * Created		12/15/13 8:04 AM 
- * Author		Matt Thomas matt@betweenbrain.com
- * Copyright	Copyright (C) 2013 betweenbrain llc. 
+ * File            router.php
+ * Created        12/15/13 8:04 AM
+ * Author        Matt Thomas matt@betweenbrain.com
+ * Copyright    Copyright (C) 2013 betweenbrain llc.
  */
- 
 
-function K2searchBuildRoute(&$query){
+function K2searchBuildRoute(&$query) {
 
+	$segments = array();
+	if (isset($query['view'])) {
+		$segments[] = $query['view'];
+		unset($query['view']);
+	}
+	if (isset($query['id'])) {
+		$segments[] = $query['id'];
+		unset($query['id']);
+	};
+
+	return $segments;
 }
 
-function K2searchParseRoute($segments){
+function K2searchParseRoute($segments) {
 
+	$vars = array();
+	switch ($segments[0]) {
+		case 'categories':
+			$vars['view'] = 'categories';
+			break;
+		case 'category':
+			$vars['view'] = 'category';
+			$id           = explode(':', $segments[1]);
+			$vars['id']   = (int) $id[0];
+			break;
+		case 'article':
+			$vars['view'] = 'article';
+			$id           = explode(':', $segments[1]);
+			$vars['id']   = (int) $id[0];
+			break;
+	}
+
+	return $vars;
 }
