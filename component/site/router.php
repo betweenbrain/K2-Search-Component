701cfa6af42b014e6ca0f552616608cb759f2d8b
Matt Thomas <matt@betweenbrain.com>
2013-12-15 08:45:24 -0500

    Adding empty router.php

---
+++ component/site/router.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * File			router.php
+ * Created		12/15/13 8:04 AM 
+ * Author		Matt Thomas matt@betweenbrain.com
+ * Copyright	Copyright (C) 2013 betweenbrain llc. 
+ */
+ 
+
+function K2searchBuildRoute(&$query){
+
+}
+
+function K2searchParseRoute($segments){
+
+}
