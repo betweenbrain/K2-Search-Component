6b2f62c2614f84a1a94e47ab72ef9a20b664c108
Matt Thomas <matt@betweenbrain.com>
2013-12-14 21:52:36 -0500

    Greatly improved term highlighting

--- component/site/models/k2search.php
+++ component/site/models/k2search.php
@@ -66,7 +66,13 @@ class K2searchModelK2search extends JModel {
 	private function highlightTerms($term, $results) {
 
 		foreach ($results as $result) {
-			$result->introtext = preg_replace('/([a-zA-Z\s])' . $term . '([a-zA-Z\s])/i', '$1<i style="background: yellow">' . $term . '</i>$2', $result->introtext);
+			if (strpos($term, ' ')) {
+				$terms = explode(' ', $term);
+				foreach ($terms as $term) {
+					$result->introtext = preg_replace('/([a-zA-Z\s])(' . $term . ')([a-zA-Z\s])/i', '$1<i style="background: yellow">$2</i>$3', $result->introtext);
+				}
+			}
+			$result->introtext = preg_replace('/([a-zA-Z\s])(' . $term . ')([a-zA-Z\s])/i', '$1<i style="background: yellow">$2</i>$3', $result->introtext);
 		}
 
 		return $results;
