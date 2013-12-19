2d6d23f8ea7e39cde0280839d2441b651386284c
Matt Thomas <matt@betweenbrain.com>
2013-12-18 20:31:03 -0500

    Basic router for play

---
+++ component/site/router.php
@@ -0,0 +1,35 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       router.php
+ * Created    12/18/13 10:32 AM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+
+function K2searchBuildRoute(&$query) {
+
+	$segments = array();
+
+	if (isset($query['term'])) {
+		$segments[] = $query['term'];
+		unset($query['term']);
+	}
+
+	return $segments;
+}
+
+function K2searchParseRoute($segments) {
+
+	$vars = array();
+
+	switch ($segments[0]) {
+		case 'k2search':
+			$vars['view'] = 'search';
+			break;
+	}
+
+	return $vars;
+}
