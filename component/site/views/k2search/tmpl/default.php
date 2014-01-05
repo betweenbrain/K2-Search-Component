6510152e8c8c96ff61ec9694ef9839f7708aa03a
Matt Thomas <matt@betweenbrain.com>
2014-01-04 20:01:09 -0500

    Adds copy/paste basis for tags list

--- component/site/views/k2search/tmpl/default.php
+++ component/site/views/k2search/tmpl/default.php
@@ -8,25 +8,115 @@
  * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
  * License    GNU GPL v3 or later
  */
+
+$tags = array();
+$tags[$this->category->name]['hash'] = '#';
+$tags[$this->category->name]['class'] = 'selected';
+$tags[$this->category->name]['compare'] = '';
+$tags[$this->category->name]['count'] = count($this->leading);
+$tags[$this->category->name]['data-filter'] = '*';
+$tags[$this->category->name]['name'] = 'All in ' . $this->category->name;
+$tags[$this->category->name]['nickname'] = 'All in ' . $this->category->name;
+foreach ($this->leading as $item)
+{
+	if (isset($item->tags) && count($item->tags))
+	{
+
+		foreach ($item->tags as $tag)
+		{
+			if (isset($tag->name, $tags))
+			{
+				$tags[$tag->name]['count']++;
+			}
+			else
+			{
+				$tags[$tag->name]['count'] = 1;
+			}
+			$tags[$tag->name]['name']        = $tag->name;
+			$tags[$tag->name]['class']       = '';
+			$tags[$tag->name]['compare']     = $tag->name;
+			$tags[$tag->name]['alias']       = JFilterOutput::stringURLSafe($tag->name);
+			$tags[$tag->name]['hash']        = '#' . JFilterOutput::stringURLSafe($tag->name);
+			$tags[$tag->name]['data-filter'] = '.' . JFilterOutput::stringURLSafe($tag->name);
+
+			// Truncate tag name at colon if it contains one
+			$tag->nickname = strpos($tag->name, ':') ? strstr($tag->name, ':', true) : $tag->name;
+			// Shorten remaining text to three words if it has more than 4
+			if (substr_count($tag->nickname, ' ') > 3)
+			{
+				$split = explode(' ', $tag->nickname, 4);
+				unset($split[3]);
+				$tag->nickname = implode(' ', $split);
+			}
+			$tags[$tag->name]['nickname'] = $tag->nickname;
+			$tags[$tag->name]['link']     = $tag->link;
+		}
+	}
+}
+$break = null;
+$count = count($tags);
+$divisor = ceil($count / 3);
 ?>
 <div class="container">
-	<div class="showing">
-		<?php if (isset($this->results['results']->term)) : ?>
-			<h3><?php echo $this->results['results']->count ?> results for "<?php echo $this->results['results']->term ?>"</h3>
-		<?php endif ?>
+	<?php if (count($tags) > 1) :
+		$app = JFactory::getApplication();
+		$doc = JFactory::getDocument();
+		$doc->addScript('http://cdn.guggenheim.org/lib/js/jquery.isotope.min.js');
+		$doc->addScript(JURI::base(true) . '/templates/' . $app->getTemplate() . '/js/jquery.video-filter-init.min.js');
+		?>
+		<div class="showing">
+			<h3>Showing</h3>
+			<select class="filters">
+				<option data-filter="date" class="selected">Recently Added</option>
+				<option data-filter="views">Most Viewed</option>
+				<option data-filter="title">Alphabetical</option>
+			</select>
 
-		<select class="filters">
-			<option data-filter="date" class="selected">Recently Added</option>
-			<option data-filter="views">Most Viewed</option>
-			<option data-filter="title">Alphabetical</option>
-		</select>
-
-		<div class="tag-lists">
-			<ul class="tag-list">
-
-			</ul>
+			<div class="tag-lists">
+				<ul class="tag-list">
+					<?php
+					usort($tags, function ($a, $b)
+					{
+						return strcmp(strtolower($a['compare']), strtolower($b['compare']));
+					});
+					$index = 0;
+					foreach ($tags as $tag)
+					{
+						if ($index > 0)
+						{
+							// Check for a 3-3-1 layout type condition
+							if (substr($count / 3, -3) == 333)
+							{
+								if (fmod($index, $divisor) == 0 && is_null($break))
+								{
+									echo ' </ul ><ul class="tag-list one-third" > ';
+									$break = 1;
+									// Shift one off of second column
+								}
+								elseif (fmod($index + 1, $divisor) == 0 && !is_null($break))
+								{
+									echo ' </ul ><ul class="tag-list one-third" > ';
+									$break = 1;
+								}
+							}
+							elseif (fmod($index, $divisor) == 0)
+							{
+								echo ' </ul ><ul class="tag-list one-third" > ';
+							}
+						}
+						?>
+						<li>
+							<a class="cloud <?php echo $tag['class'] ?>" href="<?php echo $tag['hash'] ?>" title="<?php echo $tag['name'] ?>" data-filter="<?php echo $tag['data-filter'] ?>" onClick="_gaq.push(['_trackEvent', 'Interaction', 'Video', 'Showing Filter: <?php echo $tag['nickname'] ?>']);"><?php echo $tag['nickname'] ?>
+								<span class="count"><?php echo $tag['count'] ?></span>
+							</a>
+						</li>
+						<?php
+						$index++;
+					} ?>
+				</ul>
+			</div>
 		</div>
-	</div>
+	<?php endif ?>
 	<div id="k2Container" class="itemListView isotope" style="position: relative; overflow: visible; height: 1344px;">
 
 		<div class="itemList clearfix">
@@ -56,7 +146,7 @@
 
 				$plugins = parse_ini_string($result->plugins, false, INI_SCANNER_RAW);
 
-				$result->tags          = $plugins['tags'];
+				$result->tags          = str_replace(',', ' ', $plugins['tags']);
 				$result->link          = '';
 				$result->videoDuration = $plugins['video_datavideoDuration'];
 				$result->videoImage    = $plugins['video_datavideoImageUrl'];
